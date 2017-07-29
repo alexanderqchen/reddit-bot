7e0643f85497dd2ec503e5fa6aa498c7ea9060fb
alexanderqchen <room2210@yahoo.com>
2017-07-29 01:06:53 -0700

    now uses multiple accounts to upvote

--- upvoteRedditBot.py
+++ upvoteRedditBot.py
@@ -3,28 +3,46 @@ import upvoteConfig
 import os
 import time
 
-user = "yoyashing"
+users = ["yoyashing", "NGNLBot"]
 
 def bot_login():
     print("Logging in...")
-    r = praw.Reddit(username = upvoteConfig.username, password = upvoteConfig.password, client_id = upvoteConfig.client_id, client_secret = upvoteConfig.client_secret, user_agent = "dog comment responder v1.0")
-    print("Login successful!\n")
-    return r
+    reddits = []
+    for i in range(len(upvoteConfig.username)):
+        print("Logging in " + upvoteConfig.username[i] + "...")
+        reddits.append(praw.Reddit(client_id=upvoteConfig.client_id[i],
+                     client_secret=upvoteConfig.client_secret[i],
+                     password=upvoteConfig.password[i],
+                     user_agent="upvotebot",
+                     username=upvoteConfig.username[i]))
+        try:
+            reddits[i].user.me()
+        except:
+            print("Unsuccessful login for " + upvoteConfig.username[i])
+            quit()
+    print("All logins successful!\n")
+    return reddits
 
-def run_bot(r, comments_replied_to):
-    print("Obtaining all comments by " + user)
-    upvoted = 0;
-    for comment in r.redditor(user).comments.new(limit=None):
-        if comment not in comments_upvoted:
-            comment.upvote()
-            upvoted += 1
-            comments_upvoted.append(comment.id)
-            with open("comments_upvoted.txt", "a") as f:
-                f.write(comment.id + "\n")
-            print("* * * Upvoted comment " + comment.id)
-            print("......Sleeping for 2 seconds......")
-            time.sleep(2)
-    print("Upvoted " + str(upvoted) + " new comments by " + user + "\n")
+def run_bot(reddits, comments_replied_to):
+    for user in users:
+        print("Obtaining all comments by " + user)
+        upvoted = 0;
+        newComments = []
+        for reddit in reddits:
+            for comment in reddit.redditor(user).comments.new(limit=None):
+                if comment not in comments_upvoted:
+                    comment.upvote()
+                    upvoted += 1
+                    if comment.id not in newComments:
+                        newComments.append(comment.id)
+                    print("* * * Upvoted comment " + comment.id)
+                    print("......Sleeping for 2 seconds......")
+                    time.sleep(2)
+        comments_upvoted.extend(newComments)
+        with open("comments_upvoted.txt", "a") as f:
+            for comment in newComments:
+                f.write(comment + "\n")
+        print("Upvoted " + str(upvoted) + " new comments by " + user + "\n")
 
 def get_saved_comments():
     if not os.path.isfile("comments_upvoted.txt"):
@@ -36,10 +54,10 @@ def get_saved_comments():
             comments_upvoted = list(filter(None, comments_upvoted))
     return comments_upvoted
 
-r = bot_login()
+reddits = bot_login()
 comments_upvoted = get_saved_comments()
 
 while True:
-    run_bot(r, comments_upvoted)
+    run_bot(reddits, comments_upvoted)
     print("......Sleeping for 5 seconds......\n")
     time.sleep(5)
